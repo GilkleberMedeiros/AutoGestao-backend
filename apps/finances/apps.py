f32c1edd1eb4ae8d4bba9570e8e5ad7e294db228
Gilkleber Medeiros <gikimedei2018@gmail.com>
2026-05-09 14:22:45 -0300

    fix: correctly register create_movimentation_group signal on finance app start.

--- apps/finances/apps.py
+++ apps/finances/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class FinancesConfig(AppConfig):
   name = "apps.finances"
+
+  def ready(self):
+    # Register signals on module import
+    import apps.finances.signals  # noqa
